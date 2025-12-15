e86d97f29d859563d42f8f8febb21075c6dd4ec7
Syahmi <syahmirahim99@gmail.com>
2025-12-15 20:43:24 +0800

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$di = new RecursiveDirectoryIterator(APPPATH . 'route');
+foreach (new RecursiveIteratorIterator($di) as $filename => $file) {
+    if (strpos($filename, '.php') !== false) {
+        require_once $filename;
+    }
+}
